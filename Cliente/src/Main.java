ce0213e1070f1c57e70d8100f6c70defa68165b1
YarethLeal <yarethleal65@gmail.com>
2021-06-02 22:25:13 -0600

    Interfaz de cliente

--- Cliente/src/Main.java
+++ Cliente/src/Main.java
@@ -1,9 +1,9 @@
+import GUI.InterfazTFTP;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		InterfazTFTP cliente = new InterfazTFTP();
 	}
 
 }
