b2aa17261fed1039fa4c741c076f3081909fe3f3
AracelySC <arasaenz15@gmail.com>
2021-06-06 11:43:07 -0600

    Actualizacion File

---
+++ Servidor/src/Data/ManejarFile.java
@@ -0,0 +1,43 @@
+package Data;
+
+import java.io.File;
+
+public class ManejarFile {
+
+	public static void manejarFile() {
+	}
+
+	public void CrearCarpeta(String nombre) {
+		File carpeta = new File("../Servidor/Clientes/" + nombre);
+		System.out.println(carpeta.getName());
+		if (!carpeta.exists()) {
+			if (carpeta.mkdirs()) {
+				System.out.println("Carpeta Creada");
+			} else {
+				System.out.println("La carpeta no se pudo crear");
+			}
+		}
+	}
+
+	public String verArchivos(String nombre) {
+		File carpeta = new File("../Servidor/Clientes/" + nombre);
+		File[] archivos = null;
+		String listaArchivos = "";
+		if (carpeta.exists()) {
+			
+			if (carpeta.isDirectory()) {
+				archivos = carpeta.listFiles();
+				for (int i = 0; i < archivos.length; i++) {
+					listaArchivos += archivos[i].getName() + "\n";
+					
+				}
+			}
+		} else {
+			System.out.println("No hay archivos o la carpeta no existe");
+		}
+
+		return listaArchivos;
+
+	}
+
+}
--- Servidor/src/Main.java
+++ Servidor/src/Main.java
@@ -3,6 +3,7 @@ import java.sql.SQLException;
 
 import javax.swing.JFrame;
 
+import Data.ManejarFile;
 import Domain.Servidor;
 
 
@@ -11,11 +12,15 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-	        Servidor servidor = new Servidor(6973);
-	        servidor.setVisible(true);
-	        servidor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        servidor.setLocationRelativeTo(null);
-	        servidor.setResizable(false);
+		ManejarFile prueba= new ManejarFile();
+//		prueba.CrearCarpeta("Ara");
+		System.out.println(prueba.verArchivos("Ara"));
+		
+//	        Servidor servidor = new Servidor(6973);
+//	        servidor.setVisible(true);
+//	        servidor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//	        servidor.setLocationRelativeTo(null);
+//	        servidor.setResizable(false);
 
 		}
 
