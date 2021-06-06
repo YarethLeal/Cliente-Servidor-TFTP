8efdcf9396957724b26b58f1b1ef34e556dd7fc9
AracelySC <arasaenz15@gmail.com>
2021-06-05 20:10:11 -0600

    Servidor para multiples clientes

--- Servidor/src/Domain/ServidorAtiende.java
+++ Servidor/src/Domain/ServidorAtiende.java
@@ -1,5 +1,36 @@
 package Domain;
 
-public class ServidorAtiende {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
+public class ServidorAtiende extends Thread{
+
+
+	private int socketPortNumber;
+	private DatagramSocket socket;
+	DatagramPacket esperaAccion;
+	
+	public ServidorAtiende(DatagramSocket socket,DatagramPacket esperaAccion) {
+		super("Hilo para Cliente");
+		this.socket = socket;
+		this.esperaAccion=esperaAccion;
+		
+	}
+	
+	public void run() {
+		try {
+		System.out.println("Cliente acceptado");
+		DatagramPacket respuesta =
+		          new DatagramPacket(esperaAccion.getData(), esperaAccion.getLength(),
+		        		  esperaAccion.getAddress(), esperaAccion.getPort());
+
+		        // Enviamos la respuesta, que es un eco
+		 
+			socket.send(respuesta);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
