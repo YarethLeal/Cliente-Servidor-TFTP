86066470f831f3e9a0b03b374f5646243a9cac4b
AracelySC <arasaenz15@gmail.com>
2021-06-06 22:27:48 -0600

    Servidor para un cliente

--- Servidor/src/Main.java
+++ Servidor/src/Main.java
@@ -4,22 +4,27 @@ import java.sql.SQLException;
 import javax.swing.JFrame;
 
 import Data.ManejarFile;
+import Data.UsuarioData;
 import Domain.Servidor;
-
-
+import Domain.ServidorPrueba;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		
+	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
 
-	        Servidor servidor = new Servidor(69);
-	        servidor.setVisible(true);
-	        servidor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        servidor.setLocationRelativeTo(null);
-	        servidor.setResizable(false);
+//	        Servidor servidor = new Servidor(69);
+//	        servidor.setVisible(true);
+//	        servidor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//	        servidor.setLocationRelativeTo(null);
+//	        servidor.setResizable(false);
+//	 
 
-		}
+		ServidorPrueba servidor = new ServidorPrueba(69);
+		servidor.setVisible(true);
+		servidor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		servidor.setLocationRelativeTo(null);
+		servidor.setResizable(false);
 
 	}
 
+}
